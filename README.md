ca9c3548bbb6265d3e074d6070ed1d5c62c29997
Devin Shoemaker <devinshoe@gmail.com>
2016-09-06 12:26:56 -0500

    Refactor script arguments and parameters
    I have refactored the way the script handles arguments and parameters in order to make them more extensible and reilable.
    
    The script now supports arguments with parameters, which can be seen with the "-f|--file" flag. 
    
    The script also does not execute any flashing if the factory image file path has not been specified.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 Bash script to flash factory images to Nexus devices without wiping user data, or overwriting a custom recovery and kernel.
 
 - Flags
--b: Flash boot.img as well
--r: Flash recovery.img as well
--c: Don't delete the factory_image folder after flashing
+-f|--file:        Flash designated file (this is followed by the file's path)
+-b|--boot:        Flash boot.img as well
+-r|--recovery:    Flash recovery.img as well
+-a|--all:         Flash all images (same as -b -r)
+-dc|--dont-clean: Don't delete the factory_image folder after flashing
--- flashFactoryImage.sh
+++ flashFactoryImage.sh
@@ -1,64 +1,95 @@
 #!/bin/bash
 
 # Declare variables
-FILENAME="$1"
+FILENAME=""
 ROOT_FOLDER="factory_image/"
 IMAGE_FOLDER="image/"
 
-bflag='false'
-rflag='false'
-cflag='false'
+BOOTIMGFLAG="false"
+RECOVERIMGFLAG="false"
+CLEANFLAG="true"
 
 # Detect possible flags
-while getopts 'brc' flag; do
-	case "${flag}" in
-		b) bflag='true' ;;
-		r) rflag='true' ;;
-		c) cflag='true' ;;
-		*) error "Unexpected option ${flag}" ;;
-	esac
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -f|--file)
+    FILENAME="$2"
+    shift # past argument
+    ;;
+    -b|--boot)
+    BOOTIMGFLAG="true"
+    shift # past argument
+    ;;
+    -r|--recovery)
+    RECOVERIMGFLAG="true"
+    shift # past argument
+    ;;
+    -a|--all)
+  	BOOTIMGFLAG="true"
+  	RECOVERIMGFLAG="true"
+  	;;
+    -dc|--dont-clean)
+    CLEANFLAG="false"
+    shift # past argument
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
 done
 
-# Create the root folder if it does not exist
-if [ ! -d $ROOT_FOLDER ]; then
-	mkdir $ROOT_FOLDER
-fi
-
-# Extract factory image
-tar zxvf $FILENAME -C $ROOT_FOLDER
-cd $ROOT_FOLDER
-cd */
-unzip *.zip -d $IMAGE_FOLDER
-
-# Flash components
-./flash-base.sh
-cd $IMAGE_FOLDER
-fastboot flash system system.img
-fastboot reboot-bootloader
-sleep 5
-fastboot flash vendor vendor.img
-fastboot reboot-bootloader
-sleep 5
-
-if [ $bflag == 'true' ]
+# Check if file path has been specified
+if [ ! $FILENAME == "" ]
 	then
-		fastboot flash boot boot.img
-		fastboot reboot-bootloader
-		sleep 5
-fi
 
-if [ $rflag == 'true' ]
-	then
-		fastboot flash recovery recovery.img
-		fastboot reboot-bootloader
-		sleep 5
-fi
+	# Create the root folder if it does not exist
+	if [ ! -d $ROOT_FOLDER ]; then
+		mkdir $ROOT_FOLDER
+	fi
 
-fastboot reboot
+	# Extract factory image
+	unzip $FILENAME -d $ROOT_FOLDER
+	cd $ROOT_FOLDER
+	cd */
+	unzip *.zip -d $IMAGE_FOLDER
 
-# Remove the root folder
-if [ $cflag == 'false' ]
-	then
-		cd ../../../
-		rm -rf $ROOT_FOLDER
+	# Flash components
+	./flash-base.sh
+	cd $IMAGE_FOLDER
+	fastboot flash system system.img
+	fastboot reboot-bootloader
+	sleep 5
+	fastboot flash vendor vendor.img
+	fastboot reboot-bootloader
+	sleep 5
+
+	if [ $BOOTIMGFLAG == 'true' ]
+		then
+			fastboot flash boot boot.img
+			fastboot reboot-bootloader
+			sleep 5
+	fi
+
+	if [ $RECOVERIMGFLAG == 'true' ]
+		then
+			fastboot flash recovery recovery.img
+			fastboot reboot-bootloader
+			sleep 5
+	fi
+
+	fastboot reboot
+
+	# Remove the root folder
+	if [ $CLEANFLAG == 'true' ]
+		then
+			cd ../../../
+			rm -rf $ROOT_FOLDER
+	fi
+
+else
+	echo "No factory image file specified."
 fi
